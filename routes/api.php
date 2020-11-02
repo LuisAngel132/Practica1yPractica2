639207d809634733d39467261118444151f33a7d
LuisAngel132 <71341676+LuisAngel132@users.noreply.github.com>
2020-11-01 23:01:20 -0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,8 @@ use App\Http\Controllers\controller\controlador;
 
 
 
+
+
 Route::post('insertar','cruid\controlador@Insertar')->middleware(['Edadmiddleware','Personasconcategoriamiddleware']);
 Route::middleware('auth:sanctum')->get('ver', 'cruid\controlador@Vista');
 Route::middleware('auth:sanctum')->delete('borrar/{id}','cruid\controlador@Borrarpersona')->where (['id'=>'[0-9]+']);
@@ -25,15 +27,16 @@ Route::middleware('auth:sanctum')->post('insertarproducto','cruid\controlador@In
 Route::middleware('auth:sanctum')->get('verproducto','cruid\controlador@Verproducto');
 Route::middleware('auth:sanctum')->delete('borrarproducto/{id}','cruid\controlador@Eliminarproducto')->where (['id'=>'[0-9]+']);
 Route::middleware('auth:sanctum')->put('actualizarproducto/{id}','cruid\controlador@Actualizarproducto')->where (['id'=>'[0-9]+'])->middleware('Esradodelproductomiddleware');;
-Route::middleware('auth:sanctum')->post('insertarcomentario','cruid\controlador@Insertarcomentario')->middleware('categoriadelforo');
+Route::middleware('auth:sanctum')->post('insertarcomentario','cruid\controlador@Insertarcomentario');
 Route::middleware('auth:sanctum')->get('vercomentarios','cruid\controlador@Vercomentarios');
 Route::middleware('auth:sanctum')->get('vercomentariosporproducto/{id?}','cruid\controlador@Vercomentariosporproducto')->where (['id'=>'[0-9]+']);
 Route::middleware('auth:sanctum')->get('vercomentariosporpersona/{id?}','cruid\controlador@Vercomentariosporpersona')->where (['id'=>'[0-9]+']);
 Route::middleware('auth:sanctum')->delete('borrarcomentario/{id}','cruid\controlador@Eliminarcomentario')->where (['id'=>'[0-9]+']);
-Route::middleware('auth:sanctum')->put('actualizarcomentario/{id}','cruid\controlador@Actualizarcomentario')->where (['id'=>'[0-9]+'])->middleware('categoriadelforo');
+Route::middleware('auth:sanctum')->put('actualizarcomentario/{id}','cruid\controlador@Actualizarcomentario')->where (['id'=>'[0-9]+']);
 Route::post('iniciodesesion','cruid\controlador@LogIn');
 Route::middleware('auth:sanctum')->get('verpermisos','cruid\controlador@verpermisos');
 Route::middleware('auth:sanctum')->put('cambiarpermisos/{id}','cruid\controlador@cambiarpermisos')->where (['id'=>'[0-9]+']);
 Route::post('iniciodesesionadministradores','cruid\controlador@LogInadministrador');
 Route::middleware('auth:sanctum')->delete('salirdesesion','cruid\controlador@LogOut');
 Route::middleware('auth:sanctum')->put('quitarpermisos/{id}','cruid\controlador@quitarpermisos')->where (['id'=>'[0-9]+']);
+
